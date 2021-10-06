10a72f3fa305199f52050e51928f35c5f725c0e9
gracejgj <joycelgracej@gmail.com>
2021-10-06 15:32:29 +0800

    redesign page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    delete 'logout', to: 'devise/session#destroy'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#homepage'
